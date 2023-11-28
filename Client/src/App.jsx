bd90cdc75c9fb4dad82ca70c5f6b6cb9c69281fc
abhishekb740 <abhishekb740@gmail.com>
2023-11-28 20:48:55 +0530

    changes

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,18 +2,18 @@ import './App.css'
 
 function App() {
   const submitHandler =async(e) =>{
-    // const response = await fetch('http://localhost:5000',{
-    //   method:'POST',
-    //   headers:{
-    //     'Content-Type':'application/json'
-    //   },
-    //   body:JSON.stringify({
-    //     name:'Abhishek',
-    //     age:20
-    //   })
-    // })
-    // console.log(await response.status);
-    // console.log(await response.json());
+    const response = await fetch('http://localhost:5000',{
+      method:'POST',
+      headers:{
+        'Content-Type':'application/json'
+      },
+      body:JSON.stringify({
+        name:'Abhishek',
+        age:20
+      })
+    })
+    console.log(await response.status);
+    console.log(await response.json());
   }
   return (
     <>
