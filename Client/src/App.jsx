f52fc8143e9ada76ecb13d1e2f8d216826e01d21
abhishekb740 <abhishekb740@gmail.com>
2023-11-29 19:34:04 +0530

    Routes to the pages defined

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,39 +1,44 @@
 import './App.css'
 import ScrollToTop from "./components/scrollCorrector/scroll.jsx";
-import Navbar from "./components/Navbar/navbar.jsx";
-import Footer from "./components/Footer/footer.jsx";
 import Home from "./pages/home";
 import Flights from "./pages/flights";
-import {Button} from "@nextui-org/react";
-
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import Headers from './pages/headers.jsx';
+import Feeds from './pages/feeds.jsx';
+import AboutUs from './pages/about-us.jsx';
+import ContactUs from './pages/contact-us.jsx';
+import Location from './pages/location.jsx';
+import Profile from './pages/profile.jsx';
 
 function App() {
-  const submitHandler =async() =>{
-    // const response = await fetch('http://localhost:5000',{
-    //   method:'POST',
-    //   headers:{
-    //     'Content-Type':'application/json'
-    //   },
-    //   body:JSON.stringify({
-    //     name:'Abhishek',
-    //     age:20
-    //   })
-    // })
-    // console.log(await response.status);
-    // console.log(await response.json());
-  }
+  // const submitHandler =async() =>{
+  //   const response = await fetch('http://localhost:5000',{
+  //     method:'POST',
+  //     headers:{
+  //       'Content-Type':'application/json'
+  //     },
+  //     body:JSON.stringify({
+  //       name:'Abhishek',
+  //       age:20
+  //     })
+  //   })
+  //   console.log(await response.status);
+  //   console.log(await response.json());
+  // }
   return (
     <Router>
       <ScrollToTop>
-      {/* <Navbar /> */}
-
+      <Headers/>
       <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/flights" element={<Flights />} />
+          <Route path="/" element={<Home />}/>
+          <Route path="/flights" element={<Flights />}/>
+          <Route path='/feeds' element={<Feeds/>}/>
+          <Route path='/about-us' element={<AboutUs/>} />
+          <Route path='/contact-us' element={<ContactUs/>} />
+          <Route path='/locations' element={<Location/>} />
+          <Route path='/locations' element={<Location/>} />
+          <Route path='/profile'  element={<Profile/>} />
       </Routes>
-
-      {/* <Footer /> */}
       </ScrollToTop>
     </Router>
   );
