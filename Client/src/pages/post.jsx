5661b77a82f43ec66b82171cb7ecf753f9d89a00
abhishekb740 <abhishekb740@gmail.com>
2023-12-01 00:39:49 +0530

    post page created

--- Client/src/pages/post.jsx
+++ Client/src/pages/post.jsx
@@ -1,13 +1,30 @@
-import { Card, CardHeader, CardBody, Image } from "@nextui-org/react";
-import { Button, ButtonGroup } from "@nextui-org/react";
+import { useState } from 'react';
+import { Card, Button, Input } from "@nextui-org/react";
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 
-import { Input } from "@nextui-org/react";
-
 const Post = () => {
+    const [selectedImage, setSelectedImage] = useState(null);
+
+    const handleImageChange = (event) => {
+        const file = event.target.files[0];
+
+        if (file) {
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                setSelectedImage(reader.result);
+            };
+            reader.readAsDataURL(file);
+        }
+    };
+
+    const handleInputClick = (event) => {
+        // Reset the value of the file input to allow re-triggering the change event
+        event.target.value = null;
+    };
+
     return (
         <div>
-            <div style={{ display: 'flex', justifyContent: 'center' }} >
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
                 <Card className="py-4" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '800px', gap: '2rem' }}>
                     <div className="pb-0 pt-2 px-4 flex-col items-start" style={{ display: 'flex', alignItems: 'center' }}>
                         <div>
@@ -15,11 +32,16 @@ const Post = () => {
                         </div>
                     </div>
                     <div style={{ display: 'flex', flexDirection: 'row', width: '100%', justifyContent: 'space-around' }} >
-                        <div>
-                            <canvas style={{ border: '1px solid black', borderRadius: '10px' }}>
-                            </canvas>
+                        <div style={{ width: '50%', border: '3px solid black' }} >
+                            {selectedImage && (
+                                <img
+                                    src={selectedImage}
+                                    alt="Selected"
+                                    style={{ width: '100%', height: '200px', objectFit: 'cover' }}
+                                />
+                            )}
                         </div>
-                        <div style={{ width: '50%', display: 'flex', flexDirection: 'column', gap: '2rem' }} >
+                        <div style={{ width: '40%', display: 'flex', flexDirection: 'column', gap: '2rem' }} >
                             <Input type="text" label="Title" radius="full" />
                             <Input type="text" label="Caption" radius="full" />
                         </div>
@@ -29,18 +51,22 @@ const Post = () => {
                             <div>
                                 Choose an Image to Post:
                             </div>
-                            <Input type="file" radius="full" />
+                            <input style={{border: '1px solid black'}} type="file" onChange={handleImageChange} />
                         </div>
                     </div>
                     <div>
-                        <Button style={{ backgroundColor: '#f94566', color: 'white', width: '100%', height: '3rem', fontSize: '25px', fontWeight: 'bold' }} startContent={<CloudUploadIcon />} variant="shadow" >
+                        <Button
+                            style={{ backgroundColor: '#f94566', color: 'white', width: '100%', height: '3rem', fontSize: '25px', fontWeight: 'bold' }}
+                            startContent={<CloudUploadIcon />}
+                            variant="shadow"
+                        >
                             Create Post
                         </Button>
                     </div>
                 </Card>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Post;
