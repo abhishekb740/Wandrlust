8ef4f41b3345e427a7566e34cdfb7cf29b44c143
abhishekb740 <abhishekb740@gmail.com>
2023-11-30 22:10:51 +0530

    Create Post page css done

--- Client/src/pages/post.jsx
+++ Client/src/pages/post.jsx
@@ -1,21 +1,42 @@
 import { Card, CardHeader, CardBody, Image } from "@nextui-org/react";
+import { Button, ButtonGroup } from "@nextui-org/react";
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+
+import { Input } from "@nextui-org/react";
 
 const Post = () => {
     return (
         <div>
             <div style={{ display: 'flex', justifyContent: 'center' }} >
-                <Card className="py-4" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '800px' }}>
-                    <CardHeader className="pb-0 pt-2 px-4 flex-col items-start" style={{ display: 'flex', alignItems: 'center' }}>
+                <Card className="py-4" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '800px', gap: '2rem' }}>
+                    <div className="pb-0 pt-2 px-4 flex-col items-start" style={{ display: 'flex', alignItems: 'center' }}>
                         <div>
-                            <h1 className="font-bold text-large" style={{color: 'gray', fontSize: '40px'}}>What's in your Mind?</h1>
+                            <h1 style={{ color: 'gray', fontSize: '40px', fontWeight: 'bold' }}>What's in your mind?</h1>
                         </div>
-                    </CardHeader>
-                    <CardBody className="overflow-visible py-2" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-
-                    </CardBody>
-                    <CardHeader>
-                        <h4 className="font-bold text-large">Caption</h4>
-                    </CardHeader>
+                    </div>
+                    <div style={{ display: 'flex', flexDirection: 'row', width: '100%', justifyContent: 'space-around' }} >
+                        <div>
+                            <canvas style={{ border: '1px solid black', borderRadius: '10px' }}>
+                            </canvas>
+                        </div>
+                        <div style={{ width: '50%', display: 'flex', flexDirection: 'column', gap: '2rem' }} >
+                            <Input type="text" label="Title" radius="full" />
+                            <Input type="text" label="Caption" radius="full" />
+                        </div>
+                    </div>
+                    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%' }}>
+                        <div>
+                            <div>
+                                Choose an Image to Post:
+                            </div>
+                            <Input type="file" radius="full" />
+                        </div>
+                    </div>
+                    <div>
+                        <Button style={{ backgroundColor: '#f94566', color: 'white', width: '100%', height: '3rem', fontSize: '25px', fontWeight: 'bold' }} startContent={<CloudUploadIcon />} variant="shadow" >
+                            Create Post
+                        </Button>
+                    </div>
                 </Card>
             </div>
         </div>
