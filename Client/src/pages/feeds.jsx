54edefd59a1df63831171f414feec96f3978b0b9
abhishekb740 <abhishekb740@gmail.com>
2023-12-01 19:04:55 +0530

    Feeds page Styling completed

--- Client/src/pages/feeds.jsx
+++ Client/src/pages/feeds.jsx
@@ -1,13 +1,15 @@
-import { useEffect } from "react";
+// import { useEffect } from "react";
 import Cards from "./cards";
 import { Link } from "react-router-dom";
-import { Button, ButtonGroup } from "@nextui-org/react";
+import { Button } from "@nextui-org/react";
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
-import { Card, CardHeader, CardBody, Image } from "@nextui-org/react";
-import AccountCircle from '@mui/icons-material/AccountCircle';
-import IconButton from '@mui/material/IconButton';
+import { Card, CardHeader } from "@nextui-org/react";
+// import AccountCircle from '@mui/icons-material/AccountCircle';
+// import IconButton from '@mui/material/IconButton';
 import ProfileImage from "../assets/images/profile.png"
-
+import { Input } from "@nextui-org/react";
+import SearchIcon from '@mui/icons-material/Search';
+import TextField from '@mui/material/TextField';
 const Feeds = () => {
 
     return (
@@ -24,7 +26,7 @@ const Feeds = () => {
                         </div>
                     </CardHeader>
                 </Card>
-                <Link style={{width: '70%'}} to="/post" >
+                <Link style={{ width: '70%' }} to="/post" >
                     <Button style={{ backgroundColor: '#f94566', color: 'white', width: '100%', height: '4rem', fontSize: '25px', fontWeight: 'bold' }} startContent={<CloudUploadIcon />} variant="shadow" >
                         Create Post
                     </Button>
@@ -38,7 +40,44 @@ const Feeds = () => {
                 </div>
             </div>
             <div style={{ width: '25%', position: 'fixed', right: '0' }}  >
-                Right Side
+                <Card className="py-4" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '95%', gap: '1rem' }}>
+                    <TextField id="outlined-basic" label="Search User" variant="outlined" style={{ width: '95%' }} InputProps={{
+                        endAdornment: <SearchIcon />,
+                    }} />
+                    <CardHeader className="pb-0 pt-2 px-4 flex-col items-start" style={{ display: 'flex', flexDirection: 'column', gap: '0.9rem' }} >
+                        <div style={{ display: 'flex', justifyContent: 'space-between', width: '100%' }} >
+                            <div style={{ display: 'flex', gap: '1rem' }} >
+                                <img src={ProfileImage} width="40px" height="25px" ></img>
+                                <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly', width: '100%', }}>
+                                    <p className="text-tiny" style={{ fontSize: '18px' }} >Abhishek Bhagat</p>
+                                    <small className="text-default-500">Email</small>
+                                </div>
+                            </div>
+                            <Button style={{ backgroundColor: '#f94566', color: 'white', fontWeight: 'bold' }} variant="shadow">Follow</Button>
+                        </div>
+                        <div style={{ display: 'flex', justifyContent: 'space-between', width: '100%' }} >
+                            <div style={{ display: 'flex', gap: '1rem' }} >
+                                <img src={ProfileImage} width="40px" height="25px" ></img>
+                                <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly', width: '100%', }}>
+                                    <p className="text-tiny" style={{ fontSize: '18px' }} >Abhishek Bhagat</p>
+                                    <small className="text-default-500">Email</small>
+                                </div>
+                            </div>
+                            <Button style={{ backgroundColor: '#f94566', color: 'white', fontWeight: 'bold' }} variant="shadow">Follow</Button>
+                        </div>
+                        <div style={{ display: 'flex', justifyContent: 'space-between', width: '100%' }} >
+                            <div style={{ display: 'flex', gap: '1rem' }} >
+                                <img src={ProfileImage} width="40px" height="25px" ></img>
+                                <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly', width: '100%', }}>
+                                    <p className="text-tiny" style={{ fontSize: '18px' }} >Abhishek Bhagat</p>
+                                    <small className="text-default-500">Email</small>
+                                </div>
+                            </div>
+                            <Button style={{ backgroundColor: '#f94566', color: 'white', fontWeight: 'bold' }} variant="shadow">Follow</Button>
+                        </div>
+                    </CardHeader>
+                </Card>
+
             </div>
         </div>
     )
