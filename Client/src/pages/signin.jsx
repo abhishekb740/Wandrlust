33a9a8271d648cf7ab34403f57f5e1e38b5766a1
Mayank Singh <mayank757511@gmail.com>
2024-02-20 12:31:31 +0530

    Corrections made in login/signup

--- Client/src/pages/signin.jsx
+++ Client/src/pages/signin.jsx
@@ -1,5 +1,5 @@
 import { Link, useNavigate } from "react-router-dom";
-import { useState } from "react";
+// import { useState } from "react";
 
 const Signin = () => {
   const navigate = useNavigate();
@@ -39,11 +39,11 @@ const Signin = () => {
         <div className="flex flex-col items-center justify-center mx-auto md:h-screen lg:py-0">
           <a
             href="#"
-            className="flex items-center mb-6 text-2xl font-bold text-[#eb2168] "
+            className="flex items-center mb-6 text-2xl font-bold text-[#eb2168] pt-8"
           >
             WandrLust
           </a>
-          <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div className="w-full max-w-md bg-white rounded-lg shadow-xl dark:border md:mt-0 xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                 Sign In to WandrLust
--- Client/src/pages/signup.jsx
+++ Client/src/pages/signup.jsx
@@ -54,20 +54,23 @@ const Signup = () => {
 
   return (
     <div>
-      <section className=" bg-gray-50 bg-cover">
+      <section className="bg-gray-50 bg-cover">
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
           <a
             href="#"
-            className="flex items-center mb-6 text-2xl font-bold text-[#eb2168] pt-52 "
+            className="flex items-center mb-6 text-2xl font-bold text-[#eb2168] pt-8"
           >
             WandrLust
           </a>
-          <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div className="w-full max-w-xl bg-white rounded-lg shadow-xl dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                 Sign Up to WandrLust
               </h1>
-              <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
+              <form
+                className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6"
+                onSubmit={handleSubmit}
+              >
                 <div>
                   <label
                     htmlFor="name"
@@ -179,27 +182,29 @@ const Signup = () => {
                     Gender
                   </label>
                   <select
-                    defaultValue="male"
+                    value={formData.gender}
+                    onChange={handleInputChange}
                     name="gender"
                     id="gender"
                     className="bg-gray-50 border border-gray-300 text-gray-400 sm:text-sm rounded-lg focus:ring-[#E02168] focus:border-[#E02168] block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white "
-                    required=""
+                    required
                   >
-                    <option value="" defaultValue="male" disabled selected>
-                      Select your gender
-                    </option>
+                    <option value="">Select your gender</option>
                     <option value="male">Male</option>
                     <option value="female">Female</option>
                     <option value="other">Rather not say</option>
                   </select>
                 </div>
-                <button
-                  type="submit"
-                  className="w-full text-white bg-[#eb2168] hover:bg-[#E02168] focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-                >
-                  Sign Up
-                </button>
-                <p className="text-sm font-light text-[#eb2168] dark:text-gray-400">
+
+                <div className="col-span-2">
+                  <button
+                    type="submit"
+                    className="w-full text-white bg-[#eb2168] hover:bg-[#E02168] focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                  >
+                    Sign Up
+                  </button>
+                </div>
+                <p className="text-sm font-light text-[#eb2168] dark:text-gray-400 col-span-2">
                   Already have an account!{" "}
                   <Link
                     to="/signin"
