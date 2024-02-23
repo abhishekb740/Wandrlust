84fd68f617b4b0624c265bba8b84d7d0e85d4809
abhishekb740 <abhishekb740@gmail.com>
2024-02-23 15:37:21 +0530

    minor changes

--- Server/index.js
+++ Server/index.js
@@ -22,7 +22,6 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Create logs folder if not present
 const logsDir = path.join(__dirname, "logs");
 if (!fs.existsSync(logsDir)) {
   fs.mkdirSync(logsDir);
